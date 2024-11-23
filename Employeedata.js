df8ea932c910e627636c8b8a804780d04ac8ccc6
Gauri Phalke <phalkegauri2001@gmail.com>
2024-11-23 12:16:21 +0530

    Add files via upload

---
+++ Employeedata.js
@@ -0,0 +1,33 @@
+export const Employeedata = [
+    {
+        id:1,
+        firstName: "Dhananjay",
+        lastName: "Biawat",
+        age     : 30
+    },
+    {
+        id:2,
+        firstName: "Gauri",
+        lastName: "Phalke",
+        age     : 23
+    },
+    {
+        id:3,
+        firstName: "Pranav",
+        lastName: "Dandikwar",
+        age     : 23
+    },
+    {
+        id:4,
+        firstName: "Suraj",
+        lastName: "Sawant",
+        age     : 20
+    },
+    {
+        id:5,
+        firstName: "Dnyaneshwar",
+        lastName: "Patil",
+        age     : 20
+    },
+
+]
